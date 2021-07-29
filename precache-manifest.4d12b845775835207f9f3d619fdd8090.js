0531591c3f4420d4a06afc7f1182fc3d33eade23
epaullada-k <epaullada-k@users.noreply.github.com>
2021-07-29 23:08:26 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.4d12b845775835207f9f3d619fdd8090.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14274cfbc0105f127f2ed26e4faa6a0",
+    "url": "https://epaullada-k.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "4848680a278dd3e5929f",
+    "url": "https://epaullada-k.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "22b55c029a4fe199c474",
+    "url": "https://epaullada-k.github.io/task-management-frontend/static/js/2.1ecaabcc.chunk.js"
+  },
+  {
+    "revision": "4848680a278dd3e5929f",
+    "url": "https://epaullada-k.github.io/task-management-frontend/static/js/main.03105f3d.chunk.js"
+  },
+  {
+    "revision": "3f523c5f92853e57df5c",
+    "url": "https://epaullada-k.github.io/task-management-frontend/static/js/runtime~main.7f96cc1a.js"
+  }
+]);
